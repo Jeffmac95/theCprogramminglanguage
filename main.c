caa52b1f6735ff2d27deb3d31f568fdd76c62a19
Jeffrey <jeff_macpherson95@hotmail.com>
2022-12-01 19:46:11 -0400

    excersize 1-3, 1-4

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* main() {
+
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Fahreinheit   Celsius\n");
+
+    while(fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %15.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+} */
+main() {
+    float celsius, fahr;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+
+    printf("Celsius   Fahreinheit\n");
+
+    while(celsius <= upper) {
+        fahr = (celsius * 9.0/5.0) + 32.0;
+        printf("%3.0f %15.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
